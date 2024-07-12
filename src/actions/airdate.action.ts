e6d9be5cd216d38215b4ef6ecf83487527876427
9vult <ninevult@gmail.com>
2024-07-12 16:59:31 -0400

    Handle AniDB down case

--- src/actions/airdate.action.ts
+++ src/actions/airdate.action.ts
@@ -8,7 +8,14 @@ export const AirDate = async (anidb: string, airTime: string | undefined, episod
   const cid = process.env.ANIDB_API_CLIENT_NAME;
   const baseUrl = `http://api.anidb.net:9001/httpapi?client=${cid}&clientver=1&protover=1&request=anime&aid=${anidb}`;
 
-  const response = await fetch(baseUrl);
+  let response: Response = new Response();
+
+  try {
+    response = await fetch(baseUrl);
+  } catch (err) {
+    return t('anidbApiError', { lng });
+  }
+
   if (!response.ok) return t('anidbApiError', { lng });
   let resptext = await response.text();
   if (!resptext) return t('anidbResponseEmpty', { lng });
