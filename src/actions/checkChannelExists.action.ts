7821fba33b4e26dc6a7014ab92a4cf30510a0b68
9vult <ninevult@gmail.com>
2024-08-24 21:57:19 -0400

    Clean TS project

--- src/actions/checkChannelExists.action.ts
+++
@@ -1,7 +0,0 @@
-import { Client } from "discord.js";
-
-export const CheckChannelExists = (client: Client, channelId: string) => {
-  const channel = client.channels.cache.get(channelId);
-  if (channel) return true;
-  return false;
-}
