3dcf35c714b3ba519eb4b0c8b602677937ec3dd9
9vult <ninevult@gmail.com>
2024-08-24 21:57:46 -0400

    Initialize CS project

---
+++ Localizer/Localization.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Localizer
+{
+    /// <summary>
+    /// Definition of a localization table
+    /// </summary>
+    internal class Localization
+    {
+        /// <summary>
+        /// Locale code ("en-US", etc)
+        /// </summary>
+        public required string Locale { get; set; }
+        /// <summary>
+        /// Mapping of plural type names to truthy f(x)
+        /// </summary>
+        /// <remarks>"one" → "x = 1" ("other" → "x <> 1" implied)</remarks>
+        public required Dictionary<string, string> PluralDefinitions { get; set; }
+        /// <summary>
+        /// Mapping of lookup keys to values for singular strings
+        /// </summary>
+        /// <remarks>Can be used by both <see cref="Localizer.T(string, string, object[])"/> and <see cref="Localizer.T(string, string, Dictionary{string, object}, string)"/>.</remarks>
+        public required Dictionary<string, string> Singular { get; set; }
+        /// <summary>
+        /// Mapping of lookup keys to a map of plural strings
+        /// </summary>
+        /// <remarks>Can only be used by <see cref="Localizer.T(string, string, Dictionary{string, object}, string)"/>.</remarks>
+        public required Dictionary<string, Dictionary<string, string>> Plural { get; set; }
+    }
+}
---
+++ Nino/Nino.cs
@@ -0,0 +1,11 @@
+﻿using Localizer;
+using System.Text;
+using static Localizer.Localizer;
+
+namespace Nino
+{
+    public class Nino
+    {
+        
+    }
+}
