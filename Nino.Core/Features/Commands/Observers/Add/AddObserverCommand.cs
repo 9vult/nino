e705774f8e5e69b37cf363cc1352bfd3c275356e
9vult <me@ninevolt.moe>
2026-03-28 11:44:11 -0400

    Add observers

---
+++ Nino.Core/Features/Commands/Observers/Add/AddObserverCommand.cs
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MPL-2.0
+
+using Nino.Domain.ValueObjects;
+
+namespace Nino.Core.Features.Commands.Observers.Add;
+
+public sealed record AddObserverCommand(
+    ProjectId ProjectId,
+    GroupId GroupId,
+    UserId RequestedBy,
+    ChannelId UpdateChannelId,
+    ChannelId ReleaseChannelId,
+    RoleId? PrimaryRoleId = null,
+    RoleId? SecondaryRoleId = null,
+    RoleId? TertiaryRoleId = null
+) : ICommand;
